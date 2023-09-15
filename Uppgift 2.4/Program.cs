be0e6035f923dd2cda664053a17c62cddb65d651
Dijar06 <dize06001@utb.vaxjo.se>
2023-09-15 12:51:37 +0200

    Add project files.

---
+++ Uppgift 2.4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Uppgift2._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hur mycket tjänar person 1 i ditt företag?");
+            float x = float.Parse(Console.ReadLine());
+            Console.WriteLine("Hur mycket tjänar person 2 i ditt företag?");
+            float y = float.Parse(Console.ReadLine());
+            Console.WriteLine("Hur mycket tjänar person 3 i ditt företag?");
+            float z = float.Parse(Console.ReadLine());
+            float medel = (x + y + z) / 2;
+            Console.WriteLine("Medellönen för personalen är " + medel);
+        }
+    }
+}
